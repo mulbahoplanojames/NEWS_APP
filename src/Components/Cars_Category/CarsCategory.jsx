699c63810fbb995c04e1d467aaa5137e6cdd30fb
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-29 18:30:13 +0200

    adding come features on the all category section

--- src/Components/Cars_Category/CarsCategory.jsx
+++ src/Components/Cars_Category/CarsCategory.jsx
@@ -1,54 +1,64 @@
 import { Link } from "react-router-dom";
 
+import car1 from "/src/assets/car1.jpg";
+import car2 from "/src/assets/car2.jpg";
+import car3 from "/src/assets/car3.jpg";
+import car4 from "/src/assets/car4.jpg";
+
 const CarsCategory = () => {
   return (
     <>
-      <h1>CarsCategory</h1>
       <div className="lg:grid-cols-3 grid grid-cols-1 gap-5">
         <div className=" h-[430px] rounded-lg overflow-hidden shadow-lg">
-          {/* <img src={bitcoin} alt={"bitcoin"} className="w-full h-full" /> */}
+          <img src={car2} alt={"Car image"} className="w-full h-full" />
         </div>
 
         <div className="h-fit ">
-          <div className="h-fit flex justify-between gap-3 mb-4 bg-pink-400">
-            <div className="h-[130px] w-[250px] bg-green-700 rounded-md"></div>
+          <div className="h-fit flex justify-between gap-3 mb-4 overflow-hidden">
+            <div className="h-[130px] w-[250px] bg-green-700 rounded-md overflow-hidden">
+              <img src={car3} alt={"car image"} className="w-full h-full" />
+            </div>
             <div className="">
               <h1 className="pb-2 text-base">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Consequuntur
               </h1>
-              <p className="pb-1">Oplano james Mulbah</p>
-              <p>June 5 2043, 11:23:344</p>
+              <p className="pb-1 font-semibold">Author : Solomon</p>
+              <p className="font-semibold">Date : June 5 2043, 11:23</p>
             </div>
           </div>
 
-          <div className="h-fit flex justify-between gap-3 mb-4 bg-purple-800">
-            <div className="h-[130px] w-[250px] bg-green-700 rounded-md"></div>
+          <div className="h-fit flex justify-between gap-3 mb-4">
+            <div className="h-[130px] w-[250px] rounded-md overflow-hidden">
+              <img src={car4} alt={"car image"} className="w-full h-full" />
+            </div>
             <div className="">
               <h1 className="pb-2 text-base">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Consequuntur
               </h1>
-              <p className="pb-1">Oplano james Mulbah</p>
-              <p>June 5 2043, 11:23:344</p>
+              <p className="pb-1 font-semibold">Author : Mulbah Oplano</p>
+              <p className="font-semibold">Date : June 5 2043, 11:23</p>
             </div>
           </div>
 
-          <div className="h-fit flex justify-between gap-3 bg-red-600">
-            <div className="h-[130px] w-[250px] bg-green-700 rounded-md"></div>
+          <div className="h-fit flex justify-between gap-3">
+            <div className="h-[130px] w-[250px] rounded-md overflow-hidden">
+              <img src={car1} alt={"car image"} className="w-full h-full" />
+            </div>
             <div className="">
               <h1 className="pb-2 text-base">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Consequuntur
               </h1>
-              <p className="pb-1">Oplano james Mulbah</p>
-              <p>June 5 2043, 11:23:344</p>
+              <p className="pb-1 font-semibold">Author : Abdalla </p>
+              <p className="font-semibold">Date : June 5 2043, 11:23</p>
             </div>
           </div>
         </div>
 
-        <div className="bg-red-600 rounded-md h-[430px]">
-          {/* <img src={bitcoin} alt={"bitcoin"} className="w-full h-full" /> */}
+        <div className="bg-[#eaeef3] rounded-md h-[430px] grid place-items-center text-center">
+          <h1 className="text-5xl">Aids will be provided here</h1>
         </div>
       </div>
       <Link to={"/home"}>
--- src/Components/Lifestyle_Category/LifeStyleCategory.jsx
+++ src/Components/Lifestyle_Category/LifeStyleCategory.jsx
@@ -1,12 +1,66 @@
 import { Link } from "react-router-dom";
 
+import bitcoin from "/src/assets/bitcoin.webp";
+import car from "/src/assets/car.avif";
+import food from "/src/assets/food.webp";
+import sport from "/src/assets/sport.avif";
+
 const LifeStyleCategory = () => {
   return (
     <>
-      <h1>LifeStyleCategory</h1>
-      <Link to={"/home"}>
-        <h1 className="text-3xl">All Category</h1>
-      </Link>
+      <div className="lg:grid-cols-3 grid grid-cols-1 gap-5">
+        <div className=" h-[430px] rounded-lg overflow-hidden shadow-lg">
+          <img src={sport} alt={"sports"} className="w-full h-full" />
+        </div>
+
+        <div className="h-fit ">
+          <div className="h-fit flex justify-between gap-3 mb-4">
+            <div className="h-[130px] w-[250px] rounded-md overflow-hidden">
+              <img src={car} alt={"bitcoin"} className="w-full h-full" />
+            </div>
+            <div className="">
+              <h1 className="pb-2 text-base">
+                Toyota is talking about releasing her first electric car in
+                India and we are excited to see it.
+              </h1>
+              <p className="pb-1 font-semibold">Author : Oplano james</p>
+              <p className="font-semibold">Date : June 5 2043, 11:23</p>
+            </div>
+          </div>
+
+          <div className="h-fit flex justify-between gap-3 mb-4">
+            <div className="h-[130px] w-[250px] bg-green-700 rounded-md overflow-hidden">
+              <img src={food} alt={"Food"} className="w-full h-full" />
+            </div>
+            <div className="">
+              <h1 className="pb-2 text-base">
+                All of the essential tips on what food to eat to get good health
+                and how to prepare them.
+              </h1>
+              <p className="pb-1 font-semibold">Author: Smith Lon</p>
+              <p className="font-semibold">Date: June 3 2023, 9:23</p>
+            </div>
+          </div>
+
+          <div className="h-fit flex justify-between gap-3">
+            <div className="h-[130px] w-[250px] bg-green-700 rounded-md overflow-hidden">
+              <img src={bitcoin} alt={"bitcoin"} className="w-full h-full" />
+            </div>
+            <div className="">
+              <h1 className="pb-2 text-base">
+                The ctypto market is on the verge of collapse. Here are some of
+                the latest tips ad tricks.
+              </h1>
+              <p className="pb-1 font-semibold">Author: Rechard</p>
+              <p className="font-semibold">Date: May 3 2043, 4:36</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#eaeef3] rounded-md h-[430px] grid place-items-center text-center">
+          <h1 className="text-5xl">Aids will be provided here</h1>
+        </div>
+      </div>
     </>
   );
 };
